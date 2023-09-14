3bfe7a0d96a7069116e127a856265615fa37855b
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-09-14 02:36:33 +0000

    [Update] 投稿詳細ページのいいねボタンを非同期通信形式に設定

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,19 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    favorite = Favorite.new(book_id: book.id, user_id: current_user.id)
+    @book = Book.find(params[:book_id])
+    favorite = Favorite.new(book_id: @book.id, user_id: current_user.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
+
+    # 非同期通信実装の為コメントアウト
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = Favorite.find_by(book_id: book.id, user_id: current_user.id)
+    @book = Book.find(params[:book_id])
+    favorite = Favorite.find_by(book_id: @book.id, user_id: current_user.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
+
+    # 非同期通信実装の為コメントアウト
+    # redirect_back(fallback_location: root_path)
   end
 end
